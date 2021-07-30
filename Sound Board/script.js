813e720fbf19cf35571a69ec5b62d3d1e50c7b8e
VladTimofte <vladtimofte999@gmail.com>
2021-07-30 17:18:46 +0300

    Sound BOard added mute btn

--- Sound Board/script.js
+++ Sound Board/script.js
@@ -1,4 +1,4 @@
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong', 'mute']
 
 sounds.forEach(sound => {
     const btn = document.createElement('button')
